b81b846093a920091f4ceaa55c51518cc028e1da
GillolaHemaVardhanReddy <gillolahemavardhanreddy@gmail.com>
2024-06-24 19:57:55 +0530

    binary search

--- searching/binarysearch.cpp
+++ searching/binarysearch.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool binary_search(vector<int>& a,int k){
+bool binary_search_loop(vector<int>& a,int k){
     int i = 0 , n = a.size() , j = n - 1 ;
     sort(a.begin(),a.end());
     while( i <= j ) {
@@ -17,9 +17,24 @@ bool binary_search(vector<int>& a,int k){
     return false ;
 }
 
+// iterative is better than recursive dues to sc
+
+bool binary_search_recursion(vector<int>& a,int k, int i , int j){
+    int mid = (i+j)/2;
+    if(i>j) return false;
+    if(a[mid]==k){
+        return true;
+    } else if ( a[mid] > k ) {
+        binary_search_recursion(a,k,i,mid-1);
+    } else {
+        binary_search_recursion(a,k,mid+1,j);
+    }
+}
+
 int main() {
     // code 
     vector<int> a = {1,2,4,8,10};
-    cout<<binary_search(a,8);
+    cout<<binary_search_loop(a,8);
+    cout<<binary_search_recursion(a,3,0,a.size()-1);
     return 0;
 }
